57bd2054420cbdfefcda628672d5c484cdc60fd3
gmarinsk <geoffm@txstate.edu>
2014-02-11 23:43:52 -0600

    Test project

---
+++ POSTest/src/test/edu/txstate/pos/storage/remote/Login.java
@@ -0,0 +1,64 @@
+package test.edu.txstate.pos.storage.remote;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+import edu.txstate.pos.model.User;
+import edu.txstate.pos.storage.BadPasswordException;
+import edu.txstate.pos.storage.ConnectionError;
+import edu.txstate.pos.storage.NoUserFoundException;
+import edu.txstate.pos.storage.RemoteStorage;
+
+public class Login extends AndroidTestCase {
+	
+	public void testValid() {
+		Log.d("JUNIT_TEST", "testValid");
+		User user = new User("geoff","5555");
+		RemoteStorage remote = new RemoteStorage();
+		try {
+			user = remote.login(user);
+			assertEquals(false, user.isAdmin());
+			assertEquals(true, user.isActive());
+			assertEquals(1, user.getId());
+		} catch (ConnectionError e) {
+			Log.e("JUNIT_TEST", e.getMessage());
+			assertTrue(false);
+		} catch (NoUserFoundException e) {
+			assertTrue(false);
+		} catch (BadPasswordException e) {
+			assertTrue(false);
+		}
+	}
+	
+	public void testBadPassword() {
+		Log.d("JUNIT_TEST", "testBadPassword");
+		User user = new User("geoff","WRONG");
+		RemoteStorage remote = new RemoteStorage();
+		try {
+			remote.login(user);
+			assertTrue(false);
+		} catch (ConnectionError e) {
+			assertTrue(false);
+		} catch (NoUserFoundException e) {
+			assertTrue(false);
+		} catch (BadPasswordException e) {
+			assertTrue(true);
+		}
+	}
+
+	public void testNoUser() {
+		Log.d("JUNIT_TEST", "testNoUser");
+		User user = new User("WRONGUSER","5555");
+		RemoteStorage remote = new RemoteStorage();
+		try {
+			remote.login(user);
+			assertTrue(false);
+		} catch (ConnectionError e) {
+			assertTrue(false);
+		} catch (NoUserFoundException e) {
+			assertTrue(true);
+		} catch (BadPasswordException e) {
+			assertTrue(false);
+		}
+	}
+	
+}
